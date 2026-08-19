2719ee890508f66cadc45c5fcebe8bd8c8d703bd
d6574265-sudo <d6574265@gmail.com>
2026-08-19 11:42:18 +0530

    Day-5 practice

--- pyown/day-5/code.py
+++ pyown/day-5/code.py
@@ -1,17 +1,17 @@
-#even 
+#even
 def is_even(num):
     return num%2==0
 print(is_even(4))
-print(is_even(3))
-#concatenate
+print(is_even(5))
+#concatenate two str
 str1="Hello"
 str2="World"
 result=str1+" "+str2
 print(result)
-#maximum of three nums
+#Maximum of three
 def max_of_three(a,b,c):
     return max(a,b,c)
-print(max_of_three(8,9,7))
+print(max_of_three(1,2,3))
 #count of vowels
 def count_vowels(s):
     return sum(1 for char in s if char.lower() in 'aeiou')
@@ -22,6 +22,10 @@ def factorial(n):
         return 1
     return n*factorial(n-1)
 print(factorial(5))
+#str to int
+str_num="12345"
+int_num=int(str_num)
+print(int_num)
 #area of rectangle
 def area_of_rectangle(length,width):
     return length*width
@@ -45,7 +49,23 @@ def is_prime(num):
     def is_armstrong(num):
         num_str=str(num)
         num_digits=len(num_str)
-        return num==sum(int(digit)**num_digits for digit in num_str)
+        return num==sum(int(digit)**num_digits for digits in num_str)
     print(is_armstrong(123))
     print(is_armstrong(153))
+    #leap year
+    def is_leap_year(year):
+        return (year%4==0 and year%100==0)and (year%400!=00)
+    print(is_leap_year(2020))
+    print(is_leap_year(2024)) 
+    #fibonacci
+    def fibonacci(n):
+        sequence=[0,1]
+        for i in range(2,n):
+            sequence.append(sequence[-1]+sequence[-2])
+            return sequence[:n]
+        print(fibonacci(7))
+         #decimal to binary
+    def decimal_to_binary(decimal):
+        return bin(decimal)[2:]
+    print(decimal_to_binary(10))
     
